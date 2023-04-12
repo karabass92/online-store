8303f60b46001805dc69b5effe5745006cfbc9da
karabass92 <shatveryank@gmail.com>
2023-04-12 21:50:12 +0600

    adding yandex map on contacts page

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import style from './Contacts.module.css';
+import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
+
+
+const mapData = {
+    center: [55.576569, 37.116807],
+    zoom: 16,
+};
 
 
 const Contacts = () => {
@@ -7,9 +14,14 @@ const Contacts = () => {
         <main className={style.main}>
             <h1 className={style.header}>Контакты</h1>
             <div className={style.path}>Главная — Контакты</div>
-            <div className={style.map}>
-                Карта с любой точкой
+            <div className={style.mapContainer}>
+                <YMaps>
+                    <Map defaultState={mapData} className={style.map}>
+                        <Placemark geometry={[55.576569, 37.116807]} />
+                    </Map>
+                </YMaps>
             </div>
+
             <section className={style.contactInfo}>
                 <div>
                     <h3>Телефон</h3>
@@ -26,10 +38,10 @@ const Contacts = () => {
             </section>
             <h2 className={style.formHeader}>Напишите нам</h2>
             <form action="#" className={style.sendMessageForm}>
-                <input type="text" placeholder="Имя" className={style.formInput}/>
-                <input type="email" placeholder="E-mail" className={style.formInput}/>
-                <input type="text" placeholder="Телефон" className={style.formInput}/>
-                <input type="textarea" placeholder="Сообщение" className={style.messageTextInput}/>
+                <input type="text" placeholder="Имя" className={style.formInput} />
+                <input type="email" placeholder="E-mail" className={style.formInput} />
+                <input type="text" placeholder="Телефон" className={style.formInput} />
+                <input type="textarea" placeholder="Сообщение" className={style.messageTextInput} />
                 <button className={style.formButton}>Отправить</button>
             </form>
         </main>
