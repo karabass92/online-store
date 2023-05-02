1671b8a946229e428530212aaf45991ccb44c388
karabass92 <shatveryank@gmail.com>
2023-05-02 23:45:08 +0600

    send feedback in contacts components

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import style from './Contacts.module.css';
 import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
+import axios from "axios";
 
 
 const mapData = {
@@ -14,8 +15,24 @@ const Contacts = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [telephone, setTelephone] = useState('');
-    const [messageText, setMessageText] = useState('');
+    const [message, setMessage] = useState('');
+    const [messageSent, setMessageSent] = useState(false);
 
+    const nameText = useRef(name);
+    const emailText = useRef(email);
+    const telephoneText = useRef(telephone);
+    const messageText = useRef(message);
+
+    /*--------------------------------*/
+    const onSendFeedBack = () => {
+        axios.post('http://localhost:5000/api/feedback', {
+            "name": `${name}`,
+            "email": `${email}`,
+            "telephone": `${telephone}`,
+            "text": `${message}`
+        }).then(response => (response.status === 200 && setMessageSent(true)))
+    };
+    /*--------------------------------*/
 
     return (
         <main className={style.main}>
@@ -45,12 +62,28 @@ const Contacts = () => {
             </section>
             <h2 className={style.formHeader}>Напишите нам</h2>
             <form action="#" className={style.sendMessageForm}>
-                <input type="text" placeholder="Имя" className={style.formInput} />
-                <input type="email" placeholder="E-mail" className={style.formInput} />
-                <input type="text" placeholder="Телефон" className={style.formInput} />
-                <input type="textarea" placeholder="Сообщение" className={style.messageTextInput} />
-                <button className={style.formButton}>Отправить</button>
+                <input type="text" placeholder="Имя" 
+                    className={style.formInput} 
+                    ref={nameText} 
+                    onChange={() => setName(nameText.current.value)} />
+                <input type="email" placeholder="E-mail" 
+                    className={style.formInput} 
+                    ref={emailText} 
+                    onChange={() => setEmail(emailText.current.value)}/>
+                <input type="text" placeholder="Телефон" 
+                    className={style.formInput} 
+                    ref={telephoneText} 
+                    onChange={() => setTelephone(telephoneText.current.value)} />
+                <input type="textarea" placeholder="Сообщение" 
+                    className={style.messageTextInput} 
+                    ref={messageText} 
+                    onChange={() => setMessage(messageText.current.value)} />
+                <button className={style.formButton}
+                    onClick={onSendFeedBack} >
+                        Отправить
+                </button>
             </form>
+            <div className={`${style.messageSent} ${messageSent && style.show}`}>Сообщение успешно отправлено</div>
         </main>
     );
 };
